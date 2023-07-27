937d707ddcc03d798c9ade52168d818a47c5312e
dtlzz <140613703+dtlzz@users.noreply.github.com>
2023-07-27 11:19:20 +0000

    Delete who's-gonna-pay the-bill?.py

--- who's-gonna-pay the-bill?.py
+++
@@ -1,7 +0,0 @@
-import random
-last = int(input('how many people are there? '))
-pp = input('insert the names of the people: (separated with a space)')
-p = pp.split(' ')
-n = random.randint(0,last)
-o = (p [0])
-print(f'{o} has to pay the bill, sorry friend')
